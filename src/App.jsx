5d248af30900c0738c6ee4c0ef54b53640c60920
Tomato <lilliput@Tomatos-MacBook-Air.local>
2022-09-06 17:18:59 +0530

    first change

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>this is landing page</p>
+    </div>
+  );
+}
+
+export default App;
